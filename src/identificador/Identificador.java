6d6f9672e426814106fcc912ec33d9554f06978c
Marvin <marvinejo@gmail.com>
2021-08-05 21:13:50 -0600

    Inicio

---
+++ src/identificador/Identificador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package identificador;
+
+/**
+ *
+ * @author alpha
+ */
+public class Identificador {
+    
+    public static void main(String[] args) {
+        IdentificadorVentana a1 = new IdentificadorVentana();
+        a1.setVisible(true);
+    }
+}
